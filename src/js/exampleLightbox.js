75a021482a13fcf710340703cf07983db9b6810e
SirotovAlexander <alexander.sirotov@gmail.com>
2024-02-01 01:34:25 +0200

    update

--- src/js/exampleLightbox.js
+++ src/js/exampleLightbox.js
@@ -1,15 +1,7 @@
-import * as basicLightbox from 'basiclightbox';
 const modalBtn = document.querySelector('.header__button');
-// console.dir(basicLightbox);
-
-const instance = basicLightbox.create(`
-	<h1>Dynamic Content</h1>
-	<p>You can set the content of the lightbox with JS.</p>
-`);
 
 modalBtn.addEventListener('click', openModal);
 
 function openModal(event) {
   console.log(event.target);
-  instance.show();
 }
