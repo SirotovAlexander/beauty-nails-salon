b04a23846e015407d58e99e2e1924402a8fa4c84
SirotovAlexander <116684295+SirotovAlexander@users.noreply.github.com>
2024-02-01 00:59:21 +0000

    Deploying to gh-pages from @ SirotovAlexander/beauty-nails-salon@c084d0a8fd6a7d6fcc29e5209b03e375e8e596d2 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,6 +1,7 @@
-import{b as s}from"./assets/vendor-d8571883.js";(function(){const i=document.createElement("link").relList;if(i&&i.supports&&i.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const r of t.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&n(r)}).observe(document,{childList:!0,subtree:!0});function l(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=l(e);fetch(e.href,t)}})();const c=document.querySelector(".header__button");c.addEventListener("click",d);const a=s.create(`
+import{b as d}from"./assets/vendor-d8571883.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const n of t.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&s(n)}).observe(document,{childList:!0,subtree:!0});function l(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=l(e);fetch(e.href,t)}})();const a=document.querySelector(".header__button");a.addEventListener("click",m);const r=d.create(`
 <div
   style="
+    max-width: 1200px;
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
@@ -17,5 +18,5 @@ import{b as s}from"./assets/vendor-d8571883.js";(function(){const i=document.cre
   <img src="./img/ex6.jpg" width="300" height="400" alt="example" />
   <img src="./img/ex7.jpg" width="300" height="400" alt="example" />
 </div>
-`);function d(o){a.show(),console.log(o.target)}
+`,{onShow:i=>{window.addEventListener("keydown",c)},onClose:i=>{window.removeEventListener("keydown",c)}});function m(i){r.show(),document.querySelector(".basicLightbox").addEventListener("click",()=>r.close())}function c(i){i.code==="Escape"&&r.close()}
 //# sourceMappingURL=commonHelpers.js.map
