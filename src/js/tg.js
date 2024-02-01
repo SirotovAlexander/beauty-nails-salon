982bcfc2ff361f916d8fcec3dca304e5470b56fb
SirotovAlexander <alexander.sirotov@gmail.com>
2024-02-02 01:47:46 +0200

    update
    update

--- src/js/tg.js
+++ src/js/tg.js
@@ -1,2 +1,24 @@
+import axios from 'axios';
+
 const callbackForm = document.querySelector('.callback__form');
 console.dir(callbackForm);
+
+const TOKEN = '6755016444:AAE9QL601cMD22Yv8nzXsELemNX_mJQKASQ';
+const CHAT_ID = '';
+const URL_TG = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+
+callbackForm.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let message = `<b>Контактна інформація з сайту</b> \n`;
+  message += `<b>${callbackForm.tell.value}</b>`;
+
+  axios.post(URL_TG, {
+    chat_id: CHAT_ID,
+    parse_mode: 'html',
+    text: message,
+  });
+  console.log(message);
+  callbackForm.tell.value = '';
+}
